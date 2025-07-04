97c1f8ca596324be254d265c7a11d5cec3e80ea1
Adedeji234501 <adedejiazeez1k@gmail.com>
2025-07-04 22:26:07 +0100

    add item to cart broken

--- src/js/product.js
+++ src/js/product.js
@@ -1,10 +1,24 @@
-import { setLocalStorage } from "./utils.mjs";
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  // get current products in cart
+  let products = getLocalStorage("so-cart");
+
+  // confirm if cart is empty
+  if (!products) {
+    let newProductList = products.concat(product);
+    setLocalStorage("so-cart", newProductList);
+  } else {
+    /**
+     * so cart was empty
+     * we need to create a new array
+     * and add the product to it
+     */
+    setLocalStorage("so-cart", Array(product));
+  }
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
